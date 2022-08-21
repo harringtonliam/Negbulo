d36703a4ed7455e0f586c93fb8b8d2026924e150
DESKTOP-J573CC7\harri <harringtonliam@hotmail.com>
2022-08-21 11:31:12 +0100

    Player Clothing Added

---
+++ Assets/Scripts/InventoryControl/CharacterClothing.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace RPG.InventoryControl
+{
+    public class CharacterClothing : MonoBehaviour
+    {
+        [SerializeField]
+        ClothingConfig[] clothingConfigs;
+
+
+        [Serializable]
+        public struct ClothingConfig
+        {
+            public InventoryItem inventoryItem;
+            public Transform characterMesh;
+
+        }
+
+        Equipment equipment;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            equipment = GetComponent<Equipment>();
+            equipment.equipmentUpdated += SetClothing;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void SetClothing()
+        {
+            InventoryItem bodyItem = equipment.GetItemInSlot(EquipLocation.Body);
+            for (int i = 0; i < clothingConfigs.Length; i++)
+            {
+                    if (bodyItem == clothingConfigs[i].inventoryItem)
+                    {
+                        clothingConfigs[i].characterMesh.gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        clothingConfigs[i].characterMesh.gameObject.SetActive(false);
+                    }
+            }
+
+        }
+    }
+}
+
+
